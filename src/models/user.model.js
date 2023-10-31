442b4b35355b913a06853edc69475379525b2929
sunnyDev93 <shogo093@outlook.com>
2023-10-31 21:51:49 +0100

    feat: add User schema for database modeling

---
+++ src/models/user.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default model("User", UserSchema);
